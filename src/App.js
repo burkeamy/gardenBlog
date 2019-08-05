b35a1737382562a6360f2f82f3c46e4cede91139
unknown <burkeamy30@gmail.com>
2019-08-05 16:30:20 -0400

    working on layout details

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import Header from '../src/components/Header';
 import Container from '../src/components/Container';
 import Row from '../src/components/Row';
 import Wrapper from '../src/components/Wrapper'
-import BlogPost from './components/BlogPost';
-import Col from './components/Col';
-import Navbar from './components/Navbar';
+//import BlogPost from './components/BlogPost';
+//import Col from './components/Col';
+//import Navbar from './components/Navbar';
+//import post from "./blogpost.json";
 
 class App extends Component {
+  /*state = {
+    selected: post.id,
+    title: post.title,
+    article: post.article,
+}*/
+
   render() {
   return (
     <Wrapper>
-      <Header />
-      <Navbar/>
+      <Container>
+        <Header />
+    
+      </Container>
       <Container >
         <Row>
-          <Col size = "md-5">
-            <BlogPost/>
-          </Col>
+          <h2>Introducing Little Dog Garden Blog</h2>
+          <p>What do you get when an aspiring software  and web developer has a 
+            background in landscaping? A garden blog. Add a cute little dog and a 
+            family and you get Little Dog Garden Blog. This blog hopefully will 
+            deliver helpful garden and landscaping tips as well as family friendly 
+           activities, tools of the trade, product reviews, some of my favorite 
+           plants, and popular gardens to visit. Future plans include games and 
+           how to videos.</p>
+         
         </Row>
       </Container>
     </Wrapper>
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "./style.css"
-import Col from "../Col";
+import Col from "../Col"
 
-//pokemon cards, for trading and for clicking. alsp for passing props
+
+//blog post from the json object
 function BlogPost(props) {
     return (
-        <Col size = "md-4">
-            <div className = "card">
-                <div className ="img-container" alt="Responsive image">
-                    <span onClick={() => props.changeStatus(props.id)}>
-                        <img alt = {props.name} src = {props.image} />
-                    </span>
-                </div>
-            </div>
+        <Col size = "md-8">
+            <h2>{props}</h2>,
+            <p>{props}</p>
         </Col>
+       
     );
 }
 
